393da11cfaca4d8628e83c3f9ef0d1f468004ec4
Danilo Zekovic <danilozeka93@gmail.com>
2016-04-18 18:05:42 -0400

    removed header and footer

--- js/Uploader.js
+++ js/Uploader.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-import Header from './Header'
-import PageFooter from './Footer'
 import Upload from './Upload'
 
 export default (props) => {
   return (
     <div>
-      <Header />
+
       <Upload />
-      <PageFooter />
+
     </div>
   );
 };

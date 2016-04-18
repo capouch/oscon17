734508e2f5acf3278595f209546d12772edf4bf1
Danilo Zekovic <danilozeka93@gmail.com>
2016-04-18 18:05:18 -0400

    removed header and footer

--- js/Zoomer.js
+++ js/Zoomer.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-
-import Header from './Header'
-import PageFooter from './Footer'
 import Zoom from './Zoom'
 
 export default (props) => {
   return (
     <div>
-      <Header />
+
       <Zoom
         className = "display-4"/>
-      <PageFooter />
+
     </div>
   );
 };

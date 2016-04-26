5eeb7ae6a20fbfa8e1c91596100fc022eccb3235
Brian Capouch <brianc@palaver.net>
2016-04-26 16:47:55 -0400

    Patch in ImageGallery functionality for Ryan

---
+++ js/ImageShow.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ImageGallery from 'react-image-gallery'
+
+var images = [
+  {
+    original: 'http://lorempixel.com/1000/600/nature/1/',
+    thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+    originalClass: 'featured-slide',
+    thumbnailClass: 'featured-thumb',
+    originalAlt: 'original-alt',
+    thumbnailAlt: 'thumbnail-alt',
+    description: 'Optional description...'
+  },
+  {
+    original: 'http://lorempixel.com/1000/600/nature/2/',
+    thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+  },
+  {
+    original: 'http://lorempixel.com/1000/600/nature/3/',
+    thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+  }
+];
+
+let ImageShow = React.createClass ( {
+handleSlide: function(index) {
+  console.log('Slid to ' + index);
+},
+
+render: function() {
+  return (
+    <ImageGallery
+      items={images}
+      autoPlay={true}
+      slideInterval={4000}
+      onSlide={this.handleSlide}/>
+  );
+ }
+});
+
+export default ImageShow

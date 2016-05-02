c4e9bac5e47df8b6e468238a9ad139416791a62a
Brian Capouch <brianc@palaver.net>
2016-05-02 05:01:30 -0400

    Comment and clean up

--- graphql/types/image-input.js
+++ graphql/types/image-input.js
@@ -5,6 +5,13 @@ import {
   GraphQLNonNull
 } from 'graphql';
 
+/* Note:
+  In looking at the image and image-input scripts I see I am being
+  inconsistent with when the "GraphQLNonNull" prefix is used.  This should
+  be fixed before the conference, by finding out where it is needed and then
+  taking it out where it is not!
+*/
+
 export default new GraphQLInputObjectType({
   name: 'ImageRecInput',
   fields: {

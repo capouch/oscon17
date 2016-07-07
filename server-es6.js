3d183bc9d6b82a48b4b3e6c266bd2386ac7e0303
Brian Capouch <brianc@palaver.net>
2016-07-07 03:56:58 -0400

    Alter path

--- server-es6.js
+++ server-es6.js
@@ -14,8 +14,8 @@ import bodyParser from 'body-parser'
 import schema from './graphql'
 import configRoutes from './js/routes'
 
-const privateKey = fs.readFileSync('/home/brianc/CERT/server.key'),
-  certificate = fs.readFileSync('/home/brianc/CERT/server.crt'),
+const privateKey = fs.readFileSync('/home/brianc/CERTS/server.key'),
+  certificate = fs.readFileSync('/home/brianc/CERTS/server.crt'),
   credentials = {key: privateKey, cert: certificate}
 
 const app = express(),

4cfe206b2a48ee0104fb4368901f6070c90aaf80
Brian Capouch <brianc@palaver.net>
2016-07-09 04:32:19 -0400

    Working on the "double call of onAuthStateChanged" issue

--- js/Login.js
+++ js/Login.js
@@ -30,8 +30,9 @@ export default React.createClass ( {
       isLoggedIn: false
       }
   },
-  componentDidMount: function() {
-     this.state.auth.onAuthStateChanged(this.onAuthStateChanged)
+  componentWillMount: function() {
+    // Something wrong here--this function gets called twice after second render
+    this.state.auth.onAuthStateChanged(this.onAuthStateChanged)
    },
   onAuthStateChanged: function(user) {
     if (user) {
@@ -89,36 +90,3 @@ export default React.createClass ( {
       )
     }
   })
-
-/*
-let onAuthStateChanged = function(user) {
-    if (user) { // User is signed in!
-      // Get profile pic and user's name from the Firebase user object.
-      var profilePicUrl = user.photoURL; // Only change these two lines!
-      var userName = user.displayName;   // Only change these two lines!
-
-      // Set the user's profile pic and name.
-      this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
-      this.userName.textContent = userName;
-
-      // Show user's profile and sign-out button.
-      this.userName.removeAttribute('hidden');
-      this.userPic.removeAttribute('hidden');
-      this.signOutButton.removeAttribute('hidden');
-
-      // Hide sign-in button.
-      this.signInButton.setAttribute('hidden', 'true');
-
-      // We load currently existing chant messages.
-      this.loadMessages();
-    } else { // User is signed out!
-      // Hide user's profile and sign-out button.
-      this.userName.setAttribute('hidden', 'true');
-      this.userPic.setAttribute('hidden', 'true');
-      this.signOutButton.setAttribute('hidden', 'true');
-
-      // Show sign-in button.
-      this.signInButton.removeAttribute('hidden');
-    }
-  };
-*/

df65214c5059160172ab1f2360ea0e081cf95fcf
Brian Capouch <brianc@palaver.net>
2016-10-29 16:43:11 -0400

    Remove extraneous import

--- js/Upload.js
+++ js/Upload.js
@@ -4,7 +4,7 @@
  */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import DropZoneComponent from 'react-dropzone-component'
 import InfoFields from './InfoFields'
 import Confirmation from './Confirmation'

eba8e54abe58061b38792c0f0bded2b6116b91d9
Brian Capouch <brianc@palaver.net>
2016-11-01 05:26:34 -0400

    Clean up

--- js/Shell.jsx
+++ js/Shell.jsx
@@ -5,8 +5,6 @@ import React from 'react'
 import { render } from "react-dom"
 import { Router, IndexRoute, Route, Link, Redirect, browserHistory } from "react-router/es"
 
-// Main component and styling
-// import SPA from './App.js'
 import "../css/main.scss"
 
 import rootRoute from './client-routes'

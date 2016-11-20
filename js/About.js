4ae894304130efc90cbc5f7853425ee1a58059b3
Brian Capouch <brianc@palaver.net>
2016-11-20 04:59:54 -0500

    This spacing thing is about to piss me off

--- js/About.js
+++ js/About.js
@@ -11,39 +11,58 @@ export default class extends React.Component {
       <div><pre>
 
       White County Historical Society was the vision of William H. Hamelle as he
+
       presented the ideas at the White County Old Settlers meeting, August 26, 1911.
+
       A committee was appointed to organize said Society.  Members of this committee
+
       were J.B. VanBuskirk, William H. Hamelle, Bernard G. Smith, James P. Simons and
+
       Will S. Bushnell.  These gentlemen met at the office of Spencer & Hamelle on
+
       the evening of September 1, 1911 and effected the organization of the White
-      County Historical Society.  The first annual meeting was held in Library Hall, 
-      Monticello IN, April 19, 1912 .
+
+      County Historical Society.  The first annual meeting was held in Library Hall,
+
+      Monticello IN, April 19, 1912.
 
 
       On October 28, 1958, a meeting was "arranged and planned for the purpose of
+
       reactioning White County Historical Society on a permanent basis".  More than
+
       sixty persons signed on as Charter Members of the White County Historical
+
       Society.
 
 
       In 1971, after storing the museum collections at the Monticello Library for a
+
       number of years, discussion turned to the possibility of a County Museum.
-      Finally, in 1976, with gratitude to the White County Commissioners, a room was
+
+      Finally, in 1976, with gratitude to the White County Commissioners, a room wa
+      s
       set-aside in the new County Building for a museum on the second floor.
 
 
       With museum acquisitions growing, the White County Historical Society was in
+
       need of a larger space.  That space was provided with the move of the
+
       Monticello Public Library to a new building on West Broadway in 1992, leaving
+
       the Carnegie Building, on Bluff Street, known as the former Monticello Library,
+
       vacant.  After minor renovations, the Carnegie Building is the present and
+
       proud home of the White County Historical Society.
 
 
       Update: September 2010.
 
 
-      Mission Statement:      To appreciate and encourage the connection of the past
+      Mission Statement: To appreciate and encourage the connection of the past
+      
       to the future by collecting and preserving White County Indianas history.
 
 

fb7446838d5cd5ace0363b73cc22a556df91b98d
Brian Capouch <brianc@palaver.net>
2016-12-04 02:42:10 -0500

    Add fakery to drop-down menu

--- js/Header.js
+++ js/Header.js
@@ -148,15 +148,16 @@ const NavHeader = React.createClass({
             <NavItem><NavLink to="/home" className="nav-link">Home</NavLink></NavItem>
             {authNavItem}
             <NavItem><NavLink to="/history" className="nav-link">History</NavLink></NavItem>
+            <NavItem><NavLink to="/slides" className="nav-link">Slide Show</NavLink></NavItem>
+            <NavItem><NavLink to="/newsletter" className="nav-link">Newsletters</NavLink></NavItem>
             <NavItem dropdown={true}>
               <DropdownToggle>Image Galleries</DropdownToggle>
               <DropdownMenu>
-                <NavLink to="/upload" className="nav-link">Upload</NavLink>
+                <NavLink to="" className="nav-link"></NavLink>
                 <NavLink to="/browse" className="nav-link">Browse</NavLink>
+                <NavLink to="/upload" className="nav-link">Upload</NavLink>
               </DropdownMenu>
             </NavItem>
-            <NavItem><NavLink to="/slides" className="nav-link">Slide Show</NavLink></NavItem>
-            <NavItem><NavLink to="/newsletter" className="nav-link">Newsletters</NavLink></NavItem>
           </Navbar>
         </div>
       )}

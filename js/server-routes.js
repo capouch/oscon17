c8a8a69896dcb4f861e8b62dbfa1787d056e7431
Brian Capouch <brianc@palaver.net>
2017-05-04 03:10:54 -0400

    Remove unnecessary code and extraneous comments

--- js/server-routes.js
+++ js/server-routes.js
@@ -7,9 +7,6 @@ import multer from 'multer'
 import cb from 'cb'
 import webPush from 'web-push'
 
-// Set of possible notification classes
-const notifyGroups = ["watch", "warning", "closure", "amber", "police"]
-
 // Multer handles MIME multi-part uploads
 //   Configure it for this usage instance
 // const multer = require('multer'),
@@ -80,10 +77,6 @@ export default function ( router, server ) {
   // Send a notification to one or more subscribed clients
   router.get(['/sknnzix', '/sknnzix/:msg', '/sknnzix/:type/:msg'], function(req, res) {
     messageType = req.params.type
-    console.log('Test message type 1: ' + messageType)
-    if (!notifyGroups.includes(messageType))
-      messageType = null
-    console.log('Test message type 2: ' + messageType)
     customMessage = req.params.msg
     // Let the debuggers know what's going on under the hood
     console.log('Sending notifications with ' + customMessage + ' and ' + messageType)
@@ -222,7 +215,7 @@ export default function ( router, server ) {
       if (typeof customMessage !== 'undefined') {
         payload = customMessage
       }
-      console.log('At sending, we have tags of: ' + JSON.stringify(subscription.tags))
+      // console.log('At sending, we have tags of: ' + JSON.stringify(subscription.tags))
       // Code to send notify; remove item if subscription has lapsed
       if (subscription.tags.includes(messageType)) {
       const pushOptions = {

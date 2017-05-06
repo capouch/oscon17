a66d7997b251a4edc6a01081d32bc3a39913b278
Brian Capouch <brianc@palaver.net>
2017-05-06 02:53:50 -0400

    Use more meaningful name

--- public/sw.js
+++ public/sw.js
@@ -28,7 +28,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('Service worker up: ' + event.request.url)
+  console.log('Service worker got: ' + event.request.url)
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {

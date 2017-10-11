4f27c8d6801de9f0788317b1bb72f8d64b58e631
Brian Capouch <brianc@whipsaw.palaver.net>
2017-10-11 18:59:04 -0400

    Fix buglet in SW install

--- public/scripts/push.js
+++ public/scripts/push.js
@@ -15,7 +15,7 @@ var swRegistration = null;
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
 
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('/sw.js')
   .then(function(swReg) {
     console.log('Service Worker is registered', swReg);
     swRegistration = swReg
